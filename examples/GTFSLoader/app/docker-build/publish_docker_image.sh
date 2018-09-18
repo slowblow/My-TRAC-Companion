5dfb25d6b689d059524cc7f320427848994aa5b2
joangui <joan@ac.upc.edu>
2018-09-18 14:44:04 +0200

    .

--- examples/GTFSLoader/app/docker-build/publish_docker_image.sh
+++ examples/GTFSLoader/app/docker-build/publish_docker_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp target/GTFSLoader-1.0-SNAPSHOT-jar-with-dependencies.jar .
+cp ../target/GTFSLoader-1.0-SNAPSHOT-jar-with-dependencies.jar .
 docker build -t sparsitytechnologies/gtfsloader:latest .
 docker push sparsitytechnologies/gtfsloader:latest
 
