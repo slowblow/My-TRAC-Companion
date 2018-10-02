e4ed1213aab09517ac6b974f0d713bcaab19e7f8
ArnauPrat <arnau.prat@gmail.com>
2018-10-02 18:06:50 +0200

    Changed scripts to support native docker on linux hosts

--- examples/GTFSLoader/deployments/docker-compose/stop.sh
+++ examples/GTFSLoader/deployments/docker-compose/stop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == darwin ]]
+then
 eval $(docker-machine env cigo)                                                                                                       
+fi
+
 docker-compose stop
 docker-compose rm
 
