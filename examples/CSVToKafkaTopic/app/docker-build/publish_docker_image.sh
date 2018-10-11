516dc6f31567e648e5d0e55e11af72bcd7e59001
joangui <joan@ac.upc.edu>
2018-10-11 12:20:15 +0200

    publish_docker_image.sh improved

--- examples/CSVToKafkaTopic/app/docker-build/publish_docker_image.sh
+++ examples/CSVToKafkaTopic/app/docker-build/publish_docker_image.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+CURRENT_PATH=`pwd`
+
+cd ..
+mvn assembly:assembly
+
+cd $CURRENT_PATH
+
 cp ../target/CSVToKafkaTopic-1.0-SNAPSHOT-jar-with-dependencies.jar .
 docker build -t sparsitytechnologies/csvtokafkatopic:latest .
 docker push sparsitytechnologies/csvtokafkatopic:latest
