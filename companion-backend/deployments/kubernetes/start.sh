62e5cab73c3e79f4f1807879cf8585e6762e3ead
mikemaal <mike.aguila.lorente@gmail.com>
2019-05-31 11:23:31 +0200

    kubernetes companion for gcloud

--- companion-backend/deployments/kubernetes/start.sh
+++ companion-backend/deployments/kubernetes/start.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-echo "Deploying CSV TO KAFKA TOPICS module..."
+echo "Deploying Sparsity module..."
 
+
+kubectl create secret generic deploy-conf --from-file mytrac
 kubectl apply -f mysql-companion-backend-deployment.yaml
-sleep 60
+kubectl apply -f mongo-companion-backend-deployment.yaml
+sleep 10
+kubectl apply -f tracking-container-deployment.yaml
+kubectl apply -f mytrac-ws-login.yaml
+kubectl apply -f ws-mytrac-register.yaml
+kubectl apply -f ws-mytrac-trip.yaml
+kubectl apply -f ws-mytrac-activity.yaml
+kubectl apply -f firebase-container.yaml
+kubectl apply -f polls-container.yaml
+kubectl apply -f companion-backend.yaml
+kubectl apply -f mytrac-webapp.yaml
+
+kubectl apply -f mytrac-ingress.yaml
 
 
-#kubectl apply -f tracking-container-deployment.yaml
+echo "Deploying Sparsity DONE"
 
 
