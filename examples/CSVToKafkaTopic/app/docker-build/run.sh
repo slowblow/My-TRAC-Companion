e6a8fcba58b5536e39147a88f36c0ad4ed8575c7
joangui <joan@ac.upc.edu>
2019-07-09 14:47:16 +0200

    Adapted to new setJDBCSourceConnector.sh

--- examples/CSVToKafkaTopic/app/docker-build/run.sh
+++ examples/CSVToKafkaTopic/app/docker-build/run.sh
@@ -6,7 +6,16 @@ chmod +x ./ConfigurationScripts/*.sh
 
 /opt/CSVToKafkaTopic/ConfigurationScripts/waitForKafkaConenct.sh
 /opt/CSVToKafkaTopic/ConfigurationScripts/waitForMySQL.sh
-/opt/CSVToKafkaTopic/ConfigurationScripts/setJDBCConnector.sh cigo-jdbc-source_CSV_TO_KAFKA
-#/opt/CSVToKafkaTopic/ConfigurationScripts/setJDBCSinkConnector.sh 
+
+/opt/CSVToKafkaTopic/ConfigurationScripts/setJDBCSourceConnector.sh -c "cigo-jdbc-source_CSV_TO_KAFKA"\
+                                                -k $KAFKA_CONNECT_HOST\
+                                                -m $MYSQL_HOST\
+                                                -d $MYSQL_DATABASE\
+                                                -u $MYSQL_USER\
+                                                -p $MYSQL_PASSWORD\
+                                                -i "mytrac_id"\
+                                                -t "mytrac_last_modified"
+
 #/opt/CSVToKafkaTopic/ConfigurationScripts/setElasticSearchConnector.sh
+
 java -jar /opt/CSVToKafkaTopic/CSVToKafkaTopic-1.0-SNAPSHOT-jar-with-dependencies.jar -db $MYSQL_DATABASE -ip $MYSQL_HOST -sr $SCHEMA_REGISTRY_HOST
